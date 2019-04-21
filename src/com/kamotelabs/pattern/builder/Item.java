5886846a75b3298ad670a6ef9fc989dc813a38b2
Marlon Monzon <martianworm17@gmail.com>
2019-04-21 11:04:23 +0800

    Builder Pattern - Initial commit

---
+++ src/com/kamotelabs/pattern/builder/Item.java
@@ -0,0 +1,8 @@
+package com.kamotelabs.pattern.builder;
+
+public interface Item {
+
+    public String name();
+    public Packing packing();
+    public float price();
+}
---
+++ src/com/kamotelabs/pattern/builder/Packing.java
@@ -0,0 +1,6 @@
+package com.kamotelabs.pattern.builder;
+
+public interface Packing {
+
+    public String pack();
+}
